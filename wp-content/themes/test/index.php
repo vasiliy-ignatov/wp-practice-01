66ebf02aca5d9f6185d4633134802279d28b7b60
vasiliy-ignatov <inaburat@gmail.com>
2020-05-31 19:33:39 +0300

    example of the template loop

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -1,3 +1,11 @@
 <?php get_header(); ?>
 <h1>Привет, мир!</h1>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <!-- Цикл WordPress -->
+    <h4><?php the_title() ?></h4>
+<?php endwhile; else : ?>
+	<p>Записей нет.</p>
+<?php endif; ?>
+
 <?php get_footer(); ?>
