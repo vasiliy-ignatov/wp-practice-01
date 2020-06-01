33859f6d4ade4369722c88b21711ae94c2632ca1
vasiliy-ignatov <inaburat@gmail.com>
2020-06-01 23:33:15 +0300

    tags of arctile added

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -1,11 +1,24 @@
 <?php get_header(); ?>
-<h1>Привет, мир!</h1>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <!-- Цикл WordPress -->
-    <h4><?php the_title() ?></h4>
-<?php endwhile; else : ?>
-	<p>Записей нет.</p>
-<?php endif; ?>
+<div class="container">
+    <div class="row">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <!-- Цикл WordPress -->
+            <div class="col-12">
+                <div class="card">
+<!--                    <img src="..." class="card-img-top" alt="...">-->
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title() ?></h5>
+                        <p class="card-text"><?php the_content('Продолжить...') ?></p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile; else : ?>
+            <p>Записей нет.</p>
+        <?php endif; ?>
 
-<?php get_footer(); ?>
+        <?php get_footer(); ?>
+    </div>
+</div>
+
