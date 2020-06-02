888de7ca62e58e5cdaaf096d954bc2942c34f099
vasiliy-ignatov <inaburat@gmail.com>
2020-06-02 22:16:47 +0300

    permalink is added

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -1,24 +1,28 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <div class="row">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <!-- Цикл WordPress -->
-            <div class="col-12">
-                <div class="card">
+	<div class="row">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<!-- Цикл WordPress -->
+			<div class="col-12">
+				<div class="card">
 <!--                    <img src="..." class="card-img-top" alt="...">-->
-                    <div class="card-body">
-                        <h5 class="card-title"><?php the_title() ?></h5>
-                        <p class="card-text"><?php the_content('Продолжить...') ?></p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-        <?php endwhile; else : ?>
-            <p>Записей нет.</p>
-        <?php endif; ?>
+					<div class="card-body">
+						<h5 class="card-title">
+						<a href="<?php the_permalink() ?>">
+							<?php the_title() ?>
+						</a>
+						</h5>
+						<p class="card-text"><?php the_content('Продолжить...') ?></p>
+						<a href="<?php the_permalink() ?>" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+			</div>
+		<?php endwhile; else : ?>
+			<p>Записей нет.</p>
+		<?php endif; ?>
 
-        <?php get_footer(); ?>
-    </div>
+		<?php get_footer(); ?>
+	</div>
 </div>
 
